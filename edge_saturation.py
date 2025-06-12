1ec0357273d91f3fea8a0856a78d6206d819b640
Cristian Groza <cristian.groza@mail.mcgill.ca>
2025-06-12 18:51:23 -0400

    Complement edge in edge saturation

--- edge_saturation.py
+++ edge_saturation.py
@@ -8,6 +8,12 @@ cpg = open(sys.argv[2], 'r')
 
 cpgs = {}
 
+
+flip = {'>' : '<', '<' : '>'}
+
+def complement(edge):
+    return (flip[edge[2]], edge[3], flip[edge[0]], edge[1])
+
 for line in cpg:
     cpgs[line] = None
 cpg.close()
@@ -29,11 +35,14 @@ for line in gfa:
 
     nodes = re.split("([<>])", path)[1:]
 
-    edges = ["".join(nodes[i:i + 4]) for i in range(0, len(nodes), 2)][:-1]
+    edges = [tuple(nodes[i:i + 4]) for i in range(0, len(nodes), 2)][:-1]
 
     for edge in edges:
-        if edge in cpgs and cpgs[edge] is None:
+        if "".join(edge) in cpgs and cpgs[edge] is None:
             cpgs[edge] = current_hap_i
+        # handle complement edge
+        elif "".join(complement(edge)) in cpgs and cpgs[edge] is None:
+            cpgs["".join(complement(edge))] = current_hap_i
 gfa.close()
 
 
