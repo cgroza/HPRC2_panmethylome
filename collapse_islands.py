3752e4571c5722012ae3d54c66b75f297f2d0cb4
Cristian Groza <cristian.groza@mail.mcgill.ca>
2025-09-25 14:17:38 -0400

    Collapse islands/shelves/shores

---
+++ collapse_islands.py
@@ -0,0 +1,24 @@
+import gzip
+import sys
+import re
+
+islands = gzip.open(sys.argv[1], 'rt')
+
+cpg_annot = dict()
+
+def make_record():
+    return set()
+
+for line in islands:
+    cpg_conting, cpg_start, cpg_end, cpg_id, asm, asm_start, asm_end , _, _, _, _, _, _, _ = line.split('\t')
+
+    if cpg_id not in cpg_annot:
+        cpg_annot[cpg_id] = make_record()
+
+    if asm_start == '.':
+        continue
+
+    cpg_annot[cpg_id].add("T")
+
+for cpg_id in cpg_annot:
+    print(cpg_id, ";".join(cpg_annot[cpg_id]), sep = '\t')
---
+++ collapse_shelves.py
@@ -0,0 +1,24 @@
+import gzip
+import sys
+import re
+
+shelves = gzip.open(sys.argv[1], 'rt')
+
+cpg_annot = dict()
+
+def make_record():
+    return set()
+
+for line in shelves:
+    cpg_conting, cpg_start, cpg_end, cpg_id, asm, asm_start, asm_end = line.split('\t')
+
+    if cpg_id not in cpg_annot:
+        cpg_annot[cpg_id] = make_record()
+
+    if asm_start == '.':
+        continue
+
+    cpg_annot[cpg_id].add("T")
+
+for cpg_id in cpg_annot:
+    print(cpg_id, ";".join(cpg_annot[cpg_id]), sep = '\t')
