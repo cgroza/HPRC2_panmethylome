47a50bdc8d328b16a49623bd27517a682c66982b
Cristian Groza <cristian.groza@mail.mcgill.ca>
2025-09-28 20:57:11 -0400

    Used too much memory

--- collapse_segdups.py
+++ collapse_segdups.py
@@ -6,7 +6,7 @@ segdups = gzip.open(sys.argv[1], 'rt')
 cpg_annot = dict()
 
 def make_record():
-    return set()
+    return 0
 
 for line in segdups:
     cpg_contig, cpg_start, cpg_end, cpg_id, asm_contig, asm_start, asm_end, segdup_id = line.rstrip().split('\t')
@@ -18,7 +18,7 @@ for line in segdups:
         continue
 
     # cpg_annot[cpg_id].add(asm_contig + ':' + asm_start + '-' + asm_end + ':' + sv_type)
-    cpg_annot[cpg_id].add(segdup_id)
+    cpg_annot[cpg_id] += 1
 
 for cpg_id in cpg_annot:
-    print(cpg_id, len(cpg_annot[cpg_id]), sep = '\t')
+    print(cpg_id, cpg_annot[cpg_id], sep = '\t')
