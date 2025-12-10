0390afd7adb37acac60717c776b1f7eedc0ad123
Cristian Groza <cristian.groza@mail.mcgill.ca>
2025-12-10 12:55:40 -0500

    Keep track of segdup frequency by haplotype

--- collapse_segdups.py
+++ collapse_segdups.py
@@ -6,7 +6,7 @@ segdups = gzip.open(sys.argv[1], 'rt')
 cpg_annot = dict()
 
 def make_record():
-    return 0
+    return set()
 
 for line in segdups:
     cpg_contig, cpg_start, cpg_end, cpg_id, asm_contig, asm_start, asm_end, segdup_id = line.rstrip().split('\t')
@@ -17,8 +17,7 @@ for line in segdups:
     if asm_contig == '.':
         continue
 
-    # cpg_annot[cpg_id].add(asm_contig + ':' + asm_start + '-' + asm_end + ':' + sv_type)
-    cpg_annot[cpg_id] += 1
+    cpg_annot[cpg_id].add("#".join(asm_contig.split("#")[:2]))
 
 for cpg_id in cpg_annot:
-    print(cpg_id, cpg_annot[cpg_id], sep = '\t')
+    print(cpg_id, len(cpg_annot[cpg_id]), sep = '\t')
