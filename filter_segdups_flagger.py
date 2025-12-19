76ba63da2575e85103e39fc19216c7a860d3234f
Cristian Groza <cristian.groza@mail.mcgill.ca>
2025-12-19 12:18:34 -0500

    Filter segmental annotations against Flagger

---
+++ filter_segdups_flagger.py
@@ -0,0 +1,35 @@
+import intervaltree
+import sys
+import gzip
+
+segdups = open(sys.argv[1], 'r')
+flagger = gzip.open(sys.argv[2], 'rt', encoding='ascii')
+
+intervals = dict()
+
+flagger.readline()
+for line in flagger:
+    contig, start, end, *rest = line.split('\t')
+    if contig not in intervals:
+        intervals[contig] = intervaltree.IntervalTree()
+    intervals[contig][int(start): int(end)] = 1
+flagger.close()
+
+header = segdups.readline().split('\t')
+print('\t'.join(header))
+
+for line in segdups:
+    fields = dict(zip(header, line.split('\t')))
+
+    prune = False
+
+    if fields['#chr1'] in intervals:
+        if intervals[fields['#chr1']][int(fields['start1']) : int(fields['end1'])]:
+            prune = True
+
+    if fields['chr2'] in intervals:
+        if intervals[fields['chr2']][int(fields['start2']) : int(fields['end2'])]:
+            prune = True
+
+    if not prune:
+        sys.stdout.write(line)
