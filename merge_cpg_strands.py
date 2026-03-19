1d172394d808148a50682ede60dd7835814c57c2
Cristian Groza <cristian.groza@mail.mcgill.ca>
2026-03-19 09:00:04 -0400

    fix: correct loop termination to prevent infinite reading in merge_cpg_strands
    - Replace continue with break to exit loop when input lines are missing
    - Prevents infinite loop when end of input is reached

--- merge_cpg_strands.py
+++ merge_cpg_strands.py
@@ -4,7 +4,7 @@ while True:
     line1 = sys.stdin.readline()
     line2 = sys.stdin.readline()
     if not (line1 and line2):
-        continue
+        break
 
     node1, offset1, strand1, depth1, score1, cpg_id1 = line1.rstrip().split('\t')
     node2, offset2, strand2, depth2, score2, cpg_id2 = line2.rstrip().split('\t')
