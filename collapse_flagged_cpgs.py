55700ef856065c514c02a03d20622c2c29058265
Cristian Groza <cristian.groza@mail.mcgill.ca>
2026-04-03 13:01:45 -0400

    refactor: replace rbloom with BitVector for sample tracking and update logic
    - Remove rbloom dependency and use BitVector for sample annotation
    - Add sample order mapping from input file
    - Update cpg_annot to use BitVector for efficient bit counting
    - Adjust output to print count of set bits per cpg_id

--- collapse_flagged_cpgs.py
+++ collapse_flagged_cpgs.py
@@ -1,16 +1,21 @@
 import gzip
 import sys
-import rbloom
+import BitVector
 
 cpgs = gzip.open(sys.argv[1], 'rt')
+
+with open(sys.argv[2], 'rt') as samples_file:
+    i = 0
+    sample_order = {}
+    for line in samples_file:
+        sample_order[line.rstrip()] = i
+        i += 1
 
 
 cpg_annot = dict()
 
-bf = rbloom.Bloom(int(466 * 53e6), 0.01)
-
 def make_record():
-    return 0
+    return BitVector.BitVector(size = 466)
 
 for line in cpgs:
     cpg_contig, cpg_start, cpg_end, cpg_id = line.rstrip().split('\t')
@@ -22,13 +27,9 @@ for line in cpgs:
     if genome[0].startswith("CHM13") or genome[0].startswith("GRCh38"):
         genome = genome[:1]
 
-    bf_id = "#".join(genome) + "%" + cpg_id
+    sample = "#".join(genome)
 
-    if bf_id not in bf:
-        cpg_annot[cpg_id] = cpg_annot[cpg_id] + 1
-        bf.add(bf_id)
-
-    assert cpg_annot[cpg_id] > 0
+    cpg_annot[cpg_id][sample_order[sample]] = 1
 
 for cpg_id in cpg_annot:
-    print(cpg_id, cpg_annot[cpg_id], sep = '\t')
+    print(cpg_id, cpg_annot[cpg_id].count_bits(), sep = '\t')
