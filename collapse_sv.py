58f04badae55e8ce883d79e6720acd14da15b713
Cristian Groza <cristian.groza@mail.mcgill.ca>
2026-05-27 10:32:46 -0400

    refactor(collapse_sv): use full coordinate format for cpg annotation

--- collapse_sv.py
+++ collapse_sv.py
@@ -17,8 +17,8 @@ for line in sv:
     if asm_contig == '.':
         continue
 
-    # cpg_annot[cpg_id].add(asm_contig + ':' + asm_start + '-' + asm_end + ':' + sv_type)
-    cpg_annot[cpg_id].add(asm_contig + ":" + sv_type)
+    cpg_annot[cpg_id].add(asm_contig + ':' + asm_start + '-' + asm_end + ':' + sv_type)
+    # cpg_annot[cpg_id].add(asm_contig + ":" + sv_type)
 
 for cpg_id in cpg_annot:
     print(cpg_id, ";".join(cpg_annot[cpg_id]), sep = '\t')
